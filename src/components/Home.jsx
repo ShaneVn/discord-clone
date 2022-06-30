53aacd62e594c1902f6c0aa916da013998067c55
Shane <shaneboo152@gmail.com>
2022-06-30 15:01:47 -0700

    development

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,8 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
 import { Navigate } from "react-router-dom";
 import ServerIcon from "./ServerIcon";
-import { PlusIcon } from "@heroicons/react/outline";
+import { ChevronDownIcon, PlusIcon } from "@heroicons/react/outline";
+import Channel from "./Channel";
 
 function Home() {
   const [user] = useAuthState(auth);
@@ -26,6 +27,30 @@ function Home() {
           <ServerIcon image="https://cdn.iconscout.com/icon/free/png-256/javascript-2752148-2284965.png" />
           <div className="server-default hover:bg-discord_green group">
             <PlusIcon className="text-discord_green h-7 group-hover:text-white" />
+          </div>
+        </div>
+
+        <div className="bg-discord_channelsBg flex flex-col min-w-max">
+          <h2
+            className="text-white font-bold text-sm flex items-center justify-between
+          border-b border-gray-800 p-4 cursor-pointer
+          hover:bg-discord_serverNameHoverBg"
+          >
+            Official Server
+            <ChevronDownIcon className="h-5 ml-2" />
+          </h2>
+
+          <div className="text-discord_channel flex-grow first-letter:overflow-y-scroll scrollbar-hide">
+            <div className="flex items-center p-2 mb-2">
+              <ChevronDownIcon className="h-3 mr-2" />
+              <h4 className="font-semibold">channels</h4>
+              <PlusIcon className="h-6 ml-auto cursor-pointer hover:text-white" />
+              
+            </div>
+            
+            <div className="flex flex-col space-y-2 px-2 mb-4">
+              <Channel className="mb-14" />
+            </div>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,17 @@ module.exports = {
         discord_purple: "#5865f2",
         discord_green: "#3ba55c",
         discord_serverBg: "#36393f",
-        discord_serverContainer: "#202225"
+        discord_serverContainer: "#202225",
+        discord_channelsBg:"#2f3136",
+        discord_serverNameHoverBg:"#34373c",
+        discord_channel:"#8e9297"
       },
+      height: {
+        "83vh": "83vh",
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
 }
